fcdebdcf50b33295ce4568826b6ab8ac73fb8a1f
Jacqueline Zhang <jacqueline@57blocks.com>
2024-09-24 14:38:49 +0800

    add workflow

--- src/utils/api.py
+++ src/utils/api.py
@@ -1,16 +1,12 @@
 
 def send_get_request(self, path, headers):
     response = self.client.get(path, headers=headers)
-    if response.status_code == 200:
-        print("API call successful")
-    else:
+    if response.status_code != 200:
         print(f"API call failed with status code: {response.status_code}")
 
 
 def send_post_request(self, path, headers, payload):
     response = self.client.post(path, headers=headers, json=payload)
 
-    if response.status_code == 200:
-        print("POST request successful")
-    else:
+    if response.status_code != 200:
         print(f"POST request failed with status code: {response.status_code}")
