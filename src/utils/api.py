1272a2a0c46f4963b2fb83e8fd7005a605860127
Jacqueline Zhang <jacqueline@57blocks.com>
2024-09-25 13:49:30 +0800

    update test scripts

--- src/utils/api.py
+++ src/utils/api.py
@@ -3,6 +3,7 @@ def send_get_request(self, path, headers):
     response = self.client.get(path, headers=headers)
     if response.status_code != 200:
         print(f"API call failed with status code: {response.status_code}")
+        print(f"POST request failed with status code: {response.text}")
 
 
 def send_post_request(self, path, headers, payload):
@@ -10,3 +11,11 @@ def send_post_request(self, path, headers, payload):
 
     if response.status_code != 200:
         print(f"POST request failed with status code: {response.status_code}")
+        print(f"POST request failed with status code: {response.text}")
+
+def send_post_request_with_files(self, path, headers, payload, files):
+    response = self.client.post(path, headers=headers, json=payload, files=files)
+
+    if response.status_code != 200:
+        print(f"POST request failed with status code: {response.status_code}")
+        print(f"POST request failed with status code: {response.text}")
